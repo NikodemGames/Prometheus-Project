8bfcdf6b263db319a40391201c3469d880aff839
Alejandro Vaughan <k1843224@kingston.ac.uk>
2023-05-01 22:38:37 +0100

    gameover fixed
    added game over functionality game restarts on rain death again at start of game play level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 using UnityEngine.Timeline;
 
 public class GameManager : MonoBehaviour
@@ -11,30 +12,26 @@ public class GameManager : MonoBehaviour
     public PlayableDirector sceneWin;
     public Rigidbody rb;
     public GameObject enemyWin;
-    private Dictionary<string, Vector3> originalPositions = new Dictionary<string, Vector3>();
+    
 
-    private void Awake()
-    {
-        // Store the player's original position
-        originalPositions.Add("Player", rb.transform.position);
-    }
+
+
+    // Start is called before the first frame update
 
     private void Update()
     {
         if (sceneOne.isActiveAndEnabled || sceneTwo.isActiveAndEnabled || sceneWin.isActiveAndEnabled)
         {
-            // Reset the player's position to the original position
-            if (originalPositions.ContainsKey("Player"))
-            {
-                rb.transform.position = originalPositions["Player"];
-            }
             rb.isKinematic = true;
+
         }
         else
         {
             rb.isKinematic = false;
         }
+
     }
+    
 
     public void Disable()
     {
@@ -42,5 +39,12 @@ public class GameManager : MonoBehaviour
         sceneTwo.gameObject.SetActive(false);
         sceneWin.gameObject.SetActive(false);
         enemyWin.gameObject.SetActive(false);
+
     }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
